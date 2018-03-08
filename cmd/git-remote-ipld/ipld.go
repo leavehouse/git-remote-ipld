50ba514e7610ec9fda1cfda00f5843fce85caa05
leavehouse <heavelouse@gmail.com>
2018-03-08 00:41:11 -0600

    Accumulate pushed branches into a JSON IPLD object
    Pushing a branch builds a JSON IPLD object that has IPLD links to
    the pushed branch. Pushing different branches results in an IPLD
    object with links to all of the branches

--- cmd/git-remote-ipld/ipld.go
+++ cmd/git-remote-ipld/ipld.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/magik6k/git-remote-ipld/core"
+	ipfs "github.com/ipfs/go-ipfs-api"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"os"
+	"strings"
 )
+
+const repoKey = ".."
 
 type IpldHandler struct {
 	// remoteHash is hash form remote name
@@ -105,7 +110,60 @@ func (h *IpldHandler) Push(remote *core.Remote, local string, remoteRef string) 
 		return "", fmt.Errorf("push: %v", err)
 	}
 
+	repoCid, err := addRefToRepoObject(remote, remoteRef, c.String())
+	if err != nil {
+		return "", fmt.Errorf("push: %v", err)
+	}
+
 	remote.Logger.Printf("Pushed to IPFS as \x1b[32mipld::%s\x1b[39m\n", headHash)
 	remote.Logger.Printf("Head CID is %s\n", c.String())
+	remote.Logger.Printf("Repo CID is %s\n", repoCid)
 	return local, nil
+}
+
+func insertRefIntoMap(m map[string]interface{}, ref string, cid string) map[string]interface{} {
+	split := strings.Split(ref, "/")
+	currMap := m
+	for _, seg := range split {
+		if _, ok := currMap[seg]; !ok {
+			currMap[seg] = make(map[string]interface{})
+		}
+		currMap = currMap[seg].(map[string]interface{})
+	}
+	currMap["/"] = cid
+	return m
+}
+
+func addRefToRepoObject(remote *core.Remote, refName, cid string) (string, error) {
+	api := ipfs.NewLocalShell()
+
+	// store the repo object under the ".." key, works since ".." isnt allowed
+	// in git ref names
+	repoCid, err := remote.Tracker.GetRef(repoKey)
+	if err != nil {
+		return "", fmt.Errorf("push: %v", err)
+	}
+
+	repoMap := make(map[string]interface{})
+	if repoCid != nil {
+		err = api.DagGet(string(repoCid), &repoMap)
+		if err != nil {
+			// TODO: if error is nil, is repoMap guaranteed to be unchanged?
+			repoMap = make(map[string]interface{})
+		}
+	}
+
+	repoMap = insertRefIntoMap(repoMap, refName, cid)
+
+	ipldJson, err := json.Marshal(repoMap)
+	if err != nil {
+		return "", fmt.Errorf("push: %v", err)
+	}
+
+	res, err := api.DagPut(ipldJson, "json", "cbor")
+	if err != nil {
+		return "", fmt.Errorf("push: %v", err)
+	}
+
+	return res, remote.Tracker.SetRef(repoKey, []byte(res))
 }
